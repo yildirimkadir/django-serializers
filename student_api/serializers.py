f7797220ce898faaadd5cc2d4a3f87312f9e7642
yildirimkadir <yildirimkad34@gmail.com>
2022-09-17 12:10:08 +0200

    "serializers.py wurde erstellt."

---
+++ student_api/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+from .models import Student
+
+# class StudentSerializer(serializers.Serializer):
+#     first_name = serializers.CharField(max_length=30)
+#     last_name = serializers.CharField(max_length=30)
+#     number = serializers.IntegerField(required=False)
+    
+#     def create(self, validated_data):
+#         return Student.objects.create(**validated_data)
+
+#     def update(self, instance, validated_data):
+#         instance.first_name = validated_data.get('first_name', instance.first_name)
+#         instance.last_name = validated_data.get('last_name', instance.last_name)
+#         instance.number = validated_data.get('number', instance.number)
+#         instance.save()
+#         return instance
+    
+class StudentSerializer(serializers.ModelSerializer):
+    full_name= serializers.SerializerMethodField()
+    def get_full_name(self,obj):
+        return f"{obj.first_name}{obj.last_name}"
+    class Meta:
+        model = Student
+        fields = ["id", "full_name", "first_name", "last_name", "number"]
+        # fields = '__all__'
+        # exclude = ['number']
+            
